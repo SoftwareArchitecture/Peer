22d3555caed95b23f0016b7325f440871541fb07
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2013-12-16 15:10:44 +0100

    Created template for Peer project.

---
+++ src/main/java/at/ac/tuwien/softwarearchitecture/swazam/peer/App.java
@@ -0,0 +1,13 @@
+package at.ac.tuwien.softwarearchitecture.swazam.peer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
