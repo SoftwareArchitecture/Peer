4acccf411d15e8c57a56ce6ead8d58ce31e88b86
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2013-12-28 19:18:52 +0100

    observer working on folder

--- src/main/java/at/ac/tuwien/softwarearchitecture/swazam/peer/audioManager/RepositoryObserver.java
+++ src/main/java/at/ac/tuwien/softwarearchitecture/swazam/peer/audioManager/RepositoryObserver.java
@@ -1,12 +1,14 @@
 package at.ac.tuwien.softwarearchitecture.swazam.peer.audioManager;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map.Entry;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.tools.FileObject;
 
 import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
 
 import at.ac.tuwien.softwarearchitecture.swazam.peer.fingerprintExtractorAndManager.MusicFilesEvaluation;
@@ -20,45 +22,61 @@ public class RepositoryObserver{
 	}
 	public void setObservedDirectory(String path){
 		 File directory = new File(path);
+		 //System.out.println("Observed directory "+path);
 	      FileAlterationObserver observer = new FileAlterationObserver(directory);
 	      observer.addListener(new FileAlterationListener(){
 
 			@Override
 			public void onDirectoryChange(File arg0) {
+//				System.out.println(arg0.getName()+" was changed ");
+				
 				//TODO nothing for now
 			}
 
 			@Override
 			public void onDirectoryCreate(File arg0) {
-			for (Entry<AudioInputStream,String> entry:repositoryAccess.getAllFilesFromDirectory(arg0.getAbsolutePath()).entrySet())
-				musicFilesEvaluation.addFingerprint(entry.getKey(), entry.getValue());
+				//System.out.println(arg0.getName()+" was created ");
 				
+			for (Entry<AudioInputStream,String> entry:repositoryAccess.getAllFilesFromDirectory(arg0.getName()).entrySet()){
+				musicFilesEvaluation.addFingerprint(entry.getKey(), entry.getValue());
+				try {
+					entry.getKey().close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}}
 			}
 
 			@Override
 			public void onDirectoryDelete(File arg0) {
-				for (Entry<AudioInputStream,String> entry:repositoryAccess.getAllFilesFromDirectory(arg0.getAbsolutePath()).entrySet())
+				//System.out.println(arg0.getName()+" was deleted ");
+				
+				for (Entry<AudioInputStream,String> entry:repositoryAccess.getAllFilesFromDirectory(arg0.getName()).entrySet())
 					musicFilesEvaluation.removeFingerprint(entry.getValue());
 				
 			}
 
 			@Override
 			public void onFileChange(File arg0) {
+				//System.out.println(arg0.getName()+" was changed ");
+				
 				// TODO Auto-generated method stub
 				
 			}
 
 			@Override
 			public void onFileCreate(File arg0) {
+				//System.out.println(arg0.getName()+" was created ");
 				AudioInputStream audioInputStream=repositoryAccess.getFileWithPath(arg0.getAbsolutePath());
-				musicFilesEvaluation.addFingerprint(audioInputStream,arg0.getAbsolutePath());
+				musicFilesEvaluation.addFingerprint(audioInputStream,arg0.getName());
 				
 			}
 
 			@Override
 			public void onFileDelete(File arg0) {
-				AudioInputStream audioInputStream=repositoryAccess.getFileWithPath(arg0.getAbsolutePath());
-				musicFilesEvaluation.removeFingerprint(arg0.getAbsolutePath());
+				//System.out.println(arg0.getName()+" was deleted ");
+				//AudioInputStream audioInputStream=repositoryAccess.getFileWithPath(arg0.getName());
+				musicFilesEvaluation.removeFingerprint(arg0.getName());
 			}
 
 			@Override
@@ -73,5 +91,22 @@ public class RepositoryObserver{
 			}
 	    	  
 	      });
+	      try {
+			observer.initialize();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      observer.checkAndNotify();
+	      FileAlterationMonitor monitor = new FileAlterationMonitor(20);
+	      monitor.addObserver(observer);
+	      try {
+			monitor.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	     // monitor.stop();
 	}
 }
