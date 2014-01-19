f847655074e7b28ee76ccc4042fb28d3c26a802e
Georgiana Copil <e.copil@dsg.tuwien.ac.at>
2014-01-19 16:43:34 +0100

    peer manager

---
+++ src/main/java/at/ac/tuwien/softwarearchitecture/swazam/peer/peerManager/IPeerManager.java
@@ -0,0 +1,39 @@
+package at.ac.tuwien.softwarearchitecture.swazam.peer.peerManager;
+
+import java.util.Collection;
+
+import ac.at.tuwien.infosys.swa.audio.Fingerprint;
+import at.ac.tuwien.softwarearchitecture.swazam.common.infos.ClientInfo;
+import at.ac.tuwien.softwarearchitecture.swazam.common.infos.PeerFingerprintInformation;
+import at.ac.tuwien.softwarearchitecture.swazam.common.infos.PeerInfo;
+import at.ac.tuwien.softwarearchitecture.swazam.common.infos.PeerRingInformation;
+
+
+public interface IPeerManager {
+
+	public void searchFingerprint(ClientInfo client, Fingerprint fingerprint);
+
+	public void joinPeerNetwork();
+	
+	public void registerToServer();
+	
+	public void forwardSearchRequest(ClientInfo clientInfo, Fingerprint fingerprint);
+	
+	public void broadcastSuperPeerInfoHeartBeat();
+	
+	//used by SuperPeer to notify the peers that it is alive, and what is the current peer structure
+	public void updateRingInformation(PeerRingInformation peerRingInformation);
+	
+	/**
+	 * Used when a peer sends its fingerprints to the SuperPeer
+	 * @param fingerprintInformation
+	 */
+	public void distributeFingerprintsToSuperpeer (PeerFingerprintInformation fingerprintInformation);
+	
+	public PeerInfo getCurrentPeerInformation();
+	
+	public void performLeaderElection();
+	
+	
+
+}
